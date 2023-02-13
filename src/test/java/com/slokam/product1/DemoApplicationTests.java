1aff9ebe6f67b52c3745182182c4f39edb2f0766
praveen <pchary59@gmail.com>
2023-02-13 11:35:52 +0530

    Initial commit

---
+++ src/test/java/com/slokam/product1/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slokam.product1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
